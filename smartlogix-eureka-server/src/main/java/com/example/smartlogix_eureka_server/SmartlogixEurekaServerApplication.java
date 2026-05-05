47aec3f944573e45749885c4c2053ae556b1d821
victor espindola <vict.espindola@duocuc.cl>
2026-05-04 21:35:26 -0400

    corresponde al SV Eureka

---
+++ smartlogix-eureka-server/src/main/java/com/example/smartlogix_eureka_server/SmartlogixEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartlogix_eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartlogixEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartlogixEurekaServerApplication.class, args);
+	}
+
+}
